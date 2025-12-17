c56aefedd780239e9bca41bef6bab96b69482477
BOOBALAN A <136874985+boobalan22@users.noreply.github.com>
2025-12-17 22:39:27 +0530

    Add files via upload

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.ctgesa8u2e78.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin#123',
+    DB_DATABASE : 'webappdb'
+});
